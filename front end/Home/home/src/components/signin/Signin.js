6f4476d0839e6f41b506066168c3397004eb3cd6
JoachimSGL <56150685+JoachimSGL@users.noreply.github.com>
2020-03-23 16:20:11 +0100

    signin fait

--- front end/Home/home/src/components/signin/Signin.js
+++ front end/Home/home/src/components/signin/Signin.js
@@ -18,6 +18,14 @@ class SignIn extends Component {
             motDepasse :' ',
             dateNaiss : ' ',
             sexe : ' ',
+			tel : ' ',
+			genre : ' ',
+			RRPS : ' ',
+			poids : ' ',
+			localisation : ' ',
+			gp : ' ',
+			taille : ' ',
+			specialite : ' ',
            
         };
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -27,11 +35,87 @@ class SignIn extends Component {
         this.handlePasswordChange =this.handlePasswordChange.bind(this);
         this.handleDateNaiss = this.handleDateNaiss.bind(this);
         this.handleSexeChange =this.handleSexeChange.bind(this);
+		this.handleTelChange = this.handleTelChange.bind(this);
+		this.handleDateNaissChange = this.handleDateNaissChange.bind(this);
+		this.handleGenre = this.handleGenre.bind(this);
+		this.handlePoidsChange = this.handlePoidsChange.bind(this);
+		this.handleLocalisationChange = this.handleLocalisationChange.bind(this);
+		this.handleRRPSChange = this.handleRRPSChange.bind(this);
+		this.handleSpecialiteChange = this.handleSpecialiteChange.bind(this);
+		this.handleGPChange = this.handleGPChange.bind(this);
+		this.handleTailleChange = this.handleTailleChange.bind(this);
        
     }
     handleNomChange = event =>{
         this.setState({
             nom : event.target.value
+        })
+
+    };
+	handleGenre = event =>{
+        this.setState({
+            genre : event.target.value
+        })
+		var elem = document.getElementById("changement");
+		elem.innerHTML = '';
+		if(this.state.genre == "medecin"){
+			elem.innerHTML += "<input type ='text' id='specialite' placeholder= 'specialite' onChange={this.handleSpecialiteChange} className ='specialite'/>";
+			elem.innerHTML += "<input type ='text' id='localisation' placeholder= 'localisation' onChange={this.handleLocalisationChange} className ='localisation'/>";
+			elem.innerHTML += "<input type ='text' id='RRPS' placeholder= 'RRPS' onChange={this.handleRRPSChange} className ='RRPS'/>";
+		}else if(this.state.genre == "patient"){
+				elem.innerHTML += "<input type ='text' id='poids' placeholder= 'poids' onChange={this.handlePoidsChange} className ='poids'/>";
+				elem.innerHTML += "<input type ='text' id='gp' placeholder= 'groupe sanguin' onChange={this.handleGPChange} className ='gp'/>";
+				elem.innerHTML += "<input type ='number' id='taille' placeholder= 'taille' onChange={this.handleTailleChange} className ='taille'/>";
+				elem.innerHTML += "<input type ='text' id='localisation' placeholder= 'localisation' onChange={this.handleLocalisationChange} className ='localisation'/>";
+			
+		}
+
+    };
+	handleTelChange = event =>{
+        this.setState({
+            tel : event.target.value
+        })
+
+    };
+	handleGPChange = event =>{
+        this.setState({
+            gp : event.target.value
+        })
+
+    };
+	handleTailleChange = event =>{
+        this.setState({
+            taille : event.target.value
+        })
+
+    };
+	handleSpecialiteChange = event =>{
+        this.setState({
+            specialite : event.target.value
+        })
+
+    };
+	handlePoidsChange = event =>{
+        this.setState({
+            poids : event.target.value
+        })
+
+    };
+	handleLocalisationChange = event =>{
+        this.setState({
+            localisation : event.target.value
+        })
+
+    };
+	handleRRPSChange = event =>{
+        this.setState({
+            RRPS : event.target.value
+        })
+
+    };
+	handleDateNaissChange = event =>{
+        this.setState({
+            dateNaiss : event.target.value
         })
 
     };
@@ -68,15 +152,21 @@ class SignIn extends Component {
         event.preventDefault();
 		console.log(JSON.stringify(this.state));
 		fetch('http://127.0.0.1:5000/postdata', {
-			mode : 'no-cors',
+			mode : 'cors',
 			method: "POST",
 			body : JSON.stringify(this.state),
 			header : {
+				/*
 				'Accept': 'application/json',
 				'Content-Type': 'application/json',
 				'Access-Control-Allow-Origin' : '*'
+				*/
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+				'Access-Control-Allow-Origin' : '*',
+				"Access-Control-Allow-Credentials" : true 
 			}
-		}).then(reponse=>{console.log(reponse)})
+		}).then(response => response.json()).then(data=> console.log(data));
         /*
         axios
              .post('http://localhost:5000/postdata', this.state)
@@ -130,6 +220,21 @@ class SignIn extends Component {
             
                 <input type ="password" id="confpassword" placeholder= "Confirmer mot de passe *"  className="confmtp"/>
             </div>
+			<div>
+				<input type ="text" id="tel" placeholder= "telephone" onChange={this.handleTelChange} className ="tel"/>
+			</div>
+			<div>
+				<input type ="date" id="dateNaiss" onChange={this.handleDateNaissChange} className ="dateNaiss"/>
+			</div>
+			<div>
+			<input type="button" id="genre1" name="genre1" value="medecin" onClick={this.handleGenre}/>
+			<label for="genre1"> medecin</label>
+			<input type="button" id="genre2" name="genre2" value="patient" onClick={this.handleGenre}/>
+			<label for="genre2"> patient</label>
+			</div>
+			<div id="changement">
+			
+			</div>
             <hr className="ligne"></hr>
             
            <div className = "contrat">
